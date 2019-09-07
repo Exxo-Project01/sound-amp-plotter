134b182f1d846f64a4852e2e23f6c98d1d9c025d
K.K.D.A.K.Indrajith <asithaindrajithk9@gmail.com>
2019-09-07 22:26:59 +0530

    Add files via upload

---
+++ library/plotter.js
@@ -0,0 +1,45 @@
+var mic;
+var vol_history = [];
+
+function setup() {
+  canvas = createCanvas(windowWidth,windowHeight);
+  mic = new p5.AudioIn();
+  mic.start()
+  
+}
+
+function draw() {
+  background(60,179,113);
+  var factor = 100;
+  var vol = mic.getLevel()*factor;
+  vol_history.push(vol);
+  
+  for (var x = 0; x < width; x += width / 10) {
+		for (var y = 0; y < height; y += height / 10) {
+			stroke(192,192,192);
+			strokeWeight(1);
+			line(x, 0, x, height);
+			line(0, y, width, y);
+		}
+	}
+  
+  beginShape();
+  stroke(0,250,154);
+  strokeWeight(3);
+  noFill();
+  for(let i=0;i<vol_history.length;i++){
+    var y = map(vol_history[i], -1,1,height,-1);
+    vertex(i, y);
+  }
+  endShape();
+  
+  if(vol_history.length>width-(width-800)){
+     vol_history.splice(0,1) 
+  }
+  
+  
+}
+
+window.onresize = function() {
+  canvas.size(windowWidth, windowHeight);
+};
